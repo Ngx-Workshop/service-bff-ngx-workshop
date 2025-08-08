999cdd4d8bc39623bb7d5885d9a4dde893f54403
Wesley DuSell <dusellfam@gmail.com>
2025-08-08 15:30:12 -0400

    Trying process.env.MFE_ORCHESTRATOR_URL

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,23 +1,15 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { IMfeRemote } from './interfaces/mfe-remote.interface';
 
 @Injectable()
 export class AppService {
-  constructor(
-    private readonly httpService: HttpService,
-    private readonly configService: ConfigService
-  ) {}
+  constructor(private readonly httpService: HttpService) {}
 
   getMFERemotes(): Observable<IMfeRemote[]> {
-    const mfeOrchestratorUrl = this.configService.get<string>(
-      'MFE_ORCHESTRATOR_URL'
-    );
-    console.log('MFE_ORCHESTRATOR_URL:', mfeOrchestratorUrl);
-    const url = `${mfeOrchestratorUrl}/mfe-remotes`;
+    const url = `${process.env.MFE_ORCHESTRATOR_URL}/mfe-remotes`;
 
     return this.httpService
       .get<IMfeRemote[]>(url)
